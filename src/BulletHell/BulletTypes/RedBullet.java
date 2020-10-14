a0b3b7c5cdf8646e6ee1e139a1c59095cc3b21ac
Erik Borgehammar <70651965+eborgehammar@users.noreply.github.com>
2020-10-14 11:18:11 -0500

    Update RedBullet.java
    (Same as BlueBullet.java) Created a bullet object that moves around based on a user given angle and speed. The object bounces off of walls and has the hit() method which returns the element of whatever it collides with in a list (in case it collides with multiple objects)

--- src/BulletHell/BulletTypes/RedBullet.java
+++ src/BulletHell/BulletTypes/RedBullet.java
@@ -1,7 +1,116 @@
 package BulletHell.BulletTypes;
 
+import edu.macalester.graphics.*;
+
+import java.awt.Color;
+import java.util.*;
+
 import BulletHell.Bullets;
 
+
 public class RedBullet implements Bullets {
-    
+    private Ellipse shape;
+    private static final double RADIUS = 20;
+    private Random random;
+    private double xCenter, yCenter, xSpeed, ySpeed;
+    // Reference points, slightly outside of the bullet
+    private Point top, left, bottom, right;
+    private CanvasWindow canvas;
+
+    public RedBullet(CanvasWindow canvas, double initialAngle, double initialSpeed) {
+        shape = new Ellipse(500, 500, RADIUS, RADIUS);
+        shape.setFillColor(Color.RED);
+        shape.setStrokeColor(null);
+        canvas.add(shape);
+
+        xCenter = shape.getCenter().getX();
+        yCenter = shape.getCenter().getY();
+        setPoints();
+
+        double angleToRadians = Math.toRadians(initialAngle);
+        xSpeed = Math.cos(angleToRadians) * initialSpeed;
+        ySpeed = -Math.sin(angleToRadians) * initialSpeed;
+
+        this.canvas = canvas;
+    }
+
+    /*
+     * Gets the shape of the bullet
+     */
+    public Ellipse getShape() {
+        return shape;
+    }
+
+    private void setCenter(double x, double y) {
+        shape.setCenter(x, y);
+    }
+
+    private double getCenterX() {
+        return xCenter;
+    }
+
+    private double getCenterY() {
+        return yCenter;
+    }
+
+    /**
+     * Check if the ball collides with a brick, the paddle, or the walls, and 
+     * move the ball to its next position.
+     */
+    public void updatePosition() {
+        setPoints();
+        collideWalls();
+        xCenter += xSpeed;
+        yCenter += ySpeed;
+        setCenter(xCenter, yCenter);
+    }
+
+    /**
+     * Set the reference points based on the current coordinates of the ball.
+     */
+    private void setPoints() {
+        top = new Point(getCenterX(), getCenterY() - RADIUS - 0.1);
+        left = new Point(getCenterX() - RADIUS - 0.1, getCenterY());
+        bottom = new Point(getCenterX(), getCenterY() + RADIUS + 0.1);
+        right = new Point(getCenterX() + RADIUS + 0.1, getCenterY());
+    }
+
+    /**
+     * Changes the direction if the ball collides with a horizontal surface.
+     */
+    private void deflectHorizontal() {
+        ySpeed = -ySpeed;
+    }
+
+    /**
+     * Changes the direction if the ball collides with a vertical surface.
+     */
+    private void deflectVertical() {
+        xSpeed = -xSpeed;
+    }
+
+    /**
+     * Prevents the ball from getting out of the side walls and the ceiling. 
+     */
+    private void collideWalls() {
+        if ((top.getY() <= 0) || bottom.getY() >= canvas.getHeight()) {
+            deflectHorizontal();
+        }
+        if ((left.getX() <= 0) || (right.getX() >= canvas.getWidth())) {
+            deflectVertical();
+        }
+    }
+
+    /*
+     * Returns a GraphicsObject if the bullet hits something 
+     */
+    public List<GraphicsObject> hit() {
+        List<GraphicsObject> collidedElements = new ArrayList<>();
+        for (Point point : List.of(top, bottom, left, right)) {
+            if (canvas.getElementAt(point) != null) {
+                collidedElements.add(canvas.getElementAt(point));
+            }
+        }
+        return Collections.unmodifiableList(collidedElements);
+    }
 }
