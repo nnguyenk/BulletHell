323c9de493844d27e726078bd283923b005f7fcf
Erik Borgehammar <70651965+eborgehammar@users.noreply.github.com>
2020-10-14 11:16:33 -0500

    Update Bullets.java
    Added methods to the Bullets interface and comments for what to do tomorrow

--- src/BulletHell/Bullets.java
+++ src/BulletHell/Bullets.java
@@ -1,5 +1,15 @@
 package BulletHell;
 
+import java.util.List;
+
+import edu.macalester.graphics.GraphicsObject;
+
 public interface Bullets {
-    
+    public void updatePosition();
+    public List<GraphicsObject> hit();
 }
+
+// GraphicsGroup to contain all the bullet objects
+// BulletManager
+// Implement lives
+// Implement shoot?
