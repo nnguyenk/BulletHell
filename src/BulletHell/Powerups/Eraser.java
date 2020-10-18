a7e21fa70b6dc75cbc98611ddeea5a3f28601cd1
nnguyenk <nnguyen4@macalester.edu>
2020-10-18 17:36:25 -0500

    Now calls methods from Player to help manage eraser state

--- src/BulletHell/Powerups/Eraser.java
+++ src/BulletHell/Powerups/Eraser.java
@@ -3,30 +3,35 @@ package BulletHell.Powerups;
 import BulletHell.Player;
 
 public class Eraser {
+    public static final double MAX_DURATION = 5; // The maximum number of seconds the power is active.
+
     private double remainingEraser;
-    public static final double MAX_IMMUNITY = 7; // The maximum number of seconds the player is immuned.
-
-    public void StartErasing() {
-        remainingEraser = MAX_IMMUNITY;
-    }
 
     /**
-     * Returns true if the player is still immune, 
-     * and calculates the remaining time.
+     * Activate the eraser, allowing the player to absorb bullets at will.
      */
-    public boolean isErasing(Player player) {
-        if (remainingEraser <= 0) {
-           return false;
-        }
-        return true;
+    public void startErasing(Player player) {
+        remainingEraser = MAX_DURATION;
+        player.startErasing();
+    }
+
+     /**
+     * Returns true if eraser is in effect.
+     */
+    public boolean isErasing() {
+        return (remainingEraser > 0);
     }
 
     /**
-     * Reduces the remaining time of the immunity.
+     * Reduces the remaining time of the eraser effect.
+     * Ends the effect if the duration has expired.
      * 
      * @param dt The number of seconds that will be deducted from the remaining immunity.
      */
-    public void reduceErasing(double dt) {
-        remainingEraser-= dt;
-    }    
+    public void reduceErasing(double dt, Player player) {
+        remainingEraser -= dt;
+        if (!isErasing()) {
+            player.endErasing();
+        }
+    }
 }
