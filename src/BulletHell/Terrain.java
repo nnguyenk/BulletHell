feed85fdde7404936814bd90d1c87be6d1e42075
Erik Borgehammar <70651965+eborgehammar@users.noreply.github.com>
2020-10-18 10:23:46 -0500

    Update Terrain.java
    Added walls. Don't have bouncing logic yet

--- src/BulletHell/Terrain.java
+++ src/BulletHell/Terrain.java
@@ -1,5 +1,28 @@
 package BulletHell;
 
+import java.awt.Color;
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Rectangle;
+
 public class Terrain {
-    
+    CanvasWindow canvas;
+    Rectangle rectangle1 = new Rectangle(700, 750, 90, 50);
+    Rectangle rectangle2 = new Rectangle(120, 500, 20, 80);
+
+    public Terrain(CanvasWindow canvas){
+        this.canvas = canvas;
+    }
+
+    public void SummonTerrain(){
+        rectangle1.setFillColor(Color.ORANGE);
+        rectangle1.setStrokeColor(null);
+
+        rectangle2.setFillColor(Color.ORANGE);
+        rectangle2.setStrokeColor(null);
+        
+        canvas.add(rectangle1);
+        canvas.add(rectangle2);
+    }
+
 }
