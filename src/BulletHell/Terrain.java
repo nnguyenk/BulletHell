068d625bbcb7d6c3103ba4274a95b632eeebec77
Erik Borgehammar <70651965+eborgehammar@users.noreply.github.com>
2020-10-18 11:22:20 -0500

    Update Terrain.java
    Tried fixing left terrain bug

--- src/BulletHell/Terrain.java
+++ src/BulletHell/Terrain.java
@@ -8,7 +8,7 @@ import edu.macalester.graphics.Rectangle;
 public class Terrain {
     CanvasWindow canvas;
     Rectangle rectangle1 = new Rectangle(700, 720, 90, 30);
-    Rectangle rectangle2 = new Rectangle(120, 500, 20, 80);
+    Rectangle rectangle2 = new Rectangle(120, 500, 30, 80);
 
     public Terrain(CanvasWindow canvas){
         this.canvas = canvas;
