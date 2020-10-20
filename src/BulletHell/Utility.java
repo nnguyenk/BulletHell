97fa91588c789f459e2fd2026aca0792063f178b
nnguyenk <nnguyen4@macalester.edu>
2020-10-20 09:33:59 -0500

    Rewrite helper methods, now that bullets won't spawned on the player

--- src/BulletHell/Utility.java
+++ src/BulletHell/Utility.java
@@ -7,22 +7,12 @@ import edu.macalester.graphics.CanvasWindow;
 public class Utility {
     private static Random rand = new Random();
 
-    public static int randomX(CanvasWindow canvas){
-        int x;
-        do {
-           x = randomInt(100, canvas.getWidth() - 100);
-        }
-        while (x <= canvas.getCenter().getX() + 100 && x >= canvas.getCenter().getX() - 100);
-        return x;
+    public static int randomX(CanvasWindow canvas) {
+        return randomInt(50, canvas.getWidth() - 50);
     }
 
     public static int randomY(CanvasWindow canvas){
-        int y;
-        do {
-           y = randomInt(100, canvas.getHeight() - 100);
-        }
-        while (y <= canvas.getCenter().getY() + 100 && y >= canvas.getCenter().getY() - 100);
-        return y;
+        return randomInt(50, canvas.getHeight() - 50);
     }
 
     /**
