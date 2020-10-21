460857e254fc61229065343cae486c5061db3478
asolis25 <60236815+asolis25@users.noreply.github.com>
2020-10-21 12:46:43 -0500

    CONDENSING
    BETTER SMALLER FASTER

--- src/BulletHell/AnimateManager.java
+++ src/BulletHell/AnimateManager.java
@@ -16,6 +16,9 @@ public class AnimateManager {
     private boolean D = false;
     private boolean U = false;
 
+    private boolean spriteHasMoved = false;
+    private Key lastkey;
+
     ArrayList<Boolean> directions = new ArrayList<Boolean>();
     ArrayList<Image> down = new ArrayList<Image>();
     ArrayList<Image> left = new ArrayList<Image>();
@@ -41,60 +44,16 @@ public class AnimateManager {
             frameNumber = 0;
         }
         if (key == Key.LEFT_ARROW) {
-            if((R || D || U) == true && sprite != null){
-                canvas.remove(sprite);
-                frameNumber = 0;
-                R = false;
-                D = false;
-                U = false;
-            }else if(L == true && sprite != null){
-                canvas.remove(sprite);
-            }
-            sprite = left.get(frameNumber);
-            generateSprite(sprite, player);
-            L = true;
+           spriteChange(player, key, left);
         }
         if (key == Key.RIGHT_ARROW) {
-            if((L || D || U) == true && sprite != null){
-                canvas.remove(sprite);
-                frameNumber = 0;
-                L = false;
-                D = false;
-                U = false;
-            }else if(R == true && sprite != null){
-                canvas.remove(sprite);
-            }
-            sprite = right.get(frameNumber);
-            generateSprite(sprite, player);
-            R = true;
+            spriteChange(player, key, right);
         }
         if (key == Key.UP_ARROW) {
-            if((R || D || L) == true && sprite != null){
-                canvas.remove(sprite);
-                frameNumber = 0;
-                R = false;
-                D = false;
-                L = false;
-            }else if(U == true && sprite != null){
-                canvas.remove(sprite);
-            }
-            sprite = up.get(frameNumber);
-            generateSprite(sprite, player);
-            U = true;
+            spriteChange(player, key, up);
         }
         if (key == Key.DOWN_ARROW) {
-            if((R || L || U) == true && sprite != null){
-                canvas.remove(sprite);
-                frameNumber = 0;
-                R = false;
-                L = false;
-                U = false;
-            }else if(D = true && sprite != null){
-                canvas.remove(sprite);
-            }
-            sprite = down.get(frameNumber);
-            generateSprite(sprite, player);
-            D = true;
+            spriteChange(player, key, down);
         }
         getSprite();
     }
@@ -123,5 +82,19 @@ public class AnimateManager {
         for(int i = 0; i < 4; i++){
             images.add(new Image(direction + i + ".png"));
         }
+    }
+
+    private void spriteChange(Player player, Key key, ArrayList<Image> spriteSet){
+        if(spriteHasMoved == true && sprite != null && lastkey != key){
+            canvas.remove(sprite);
+            frameNumber = 0;
+            spriteHasMoved = false;
+        }else if(lastkey == key && sprite != null){
+            canvas.remove(sprite);
+        }
+        sprite = spriteSet.get(frameNumber);
+        generateSprite(sprite, player);
+        lastkey = key;
+        spriteHasMoved = true;
     }
 }
